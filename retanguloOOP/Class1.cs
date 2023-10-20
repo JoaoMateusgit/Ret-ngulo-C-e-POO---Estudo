c4b8d4572a11a7f6768b26f5cc09fb2238985319
JoaoMateusgit <joaomateusperachi33@gmail.com>
2023-10-20 13:34:58 -0300

    Adicionar arquivos de projeto.

---
+++ retanguloOOP/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace retanguloOOP
+{
+    internal class Class1
+    {
+        public double altura;
+        public double largura;
+        public double diagonal;
+
+        public double Area() { 
+            return  altura * largura;
+        }
+
+        public double Perimetro() { 
+            return 2 * (altura + largura);
+        }
+        public double Diagonal()
+        {
+            return Math.Sqrt(largura * largura + altura * altura);
+        }
+        public override string ToString()
+        {
+            return "Área: " + Area().ToString("F2", CultureInfo.InvariantCulture) + ", Perimetro: " + Perimetro().ToString("F2" ,CultureInfo.InvariantCulture) + ", Diagonal: " + Diagonal().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ retanguloOOP/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using retanguloOOP;
+using System.Globalization;
+namespace course
+{
+    class retanguloOOP
+    {
+        static void Main(String[] args)
+        {
+             Class1 a = new Class1();
+
+            Console.WriteLine("entre a altura do Retângulo: ", CultureInfo.InvariantCulture);
+            a.altura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("entre a largura do Retângulo: ", CultureInfo.InvariantCulture);
+            a.largura = double.Parse(Console.ReadLine());
+            
+    
+
+            Console.WriteLine("A área do seu retângulo é: " + a);
+        }
+    }
+}
